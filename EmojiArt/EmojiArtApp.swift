e7665eda3b246770759692c4542cc4b239014f2d
Ruslan <Alekyan_2016@mail.ru>
2022-07-25 16:51:15 +0300

    Initial commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Ruslan Alekyan on 25.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
