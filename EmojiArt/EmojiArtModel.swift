fcdcb8037d4a4434970e5eae852f979fd20f1347
Ruslan <Alekyan_2016@mail.ru>
2022-07-25 16:58:44 +0300

    Add MVVM architectural pattern to the project

---
+++ EmojiArt/EmojiArtModel.swift
@@ -0,0 +1,40 @@
+//
+//  EmojiArtModel.swift
+//  EmojiArt
+//
+//  Created by Ruslan Alekyan on 25.07.2022.
+//
+
+import Foundation
+
+struct EmojiArtModel {
+    var background = Background.blank
+    var emojis = [Emoji]()
+    
+    struct Emoji: Identifiable, Hashable {
+        let id: Int
+        let text: String
+        var x: Int
+        var y: Int
+        var size: Int
+        
+        fileprivate init(id: Int, text: String, x: Int, y: Int, size: Int) {
+            self.id = id
+            self.text = text
+            self.x = x
+            self.y = y
+            self.size = size
+        }
+    }
+    
+    init() {
+        
+    }
+    
+    private var uniqueEmojiId = 0
+    
+    mutating func addEmogi(_ text: String, at location: (x: Int, y: Int), size: Int) {
+        uniqueEmojiId += 1
+        emojis.append(Emoji(id: uniqueEmojiId, text: text, x: location.x, y: location.y, size: size))
+    }
+}
